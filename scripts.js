b630a3f66203207509106c92af670cbae0be03c4
Adam Rice <adamcoltonrice@gmail.com>
2016-10-11 15:08:32 -0600

    adds innertext to results section

--- scripts.js
+++ scripts.js
@@ -2,11 +2,24 @@ var mysteryNumber = mysteryNumber();
 var userInput = document.getElementById('user-input');
 var guess = document.getElementById('guess');
 var clear = document.getElementById('clear');
+var reset = document.getElementById('reset');
 
 console.log(mysteryNumber);
 
+clear.addEventListener('click', function() {
+  (userInput).value = '';
+});
+
+reset.addEventListener('click', function() {
+  var mysteryNumber = Math.floor((Math.random() * 100) + 1);
+  console.log(mysteryNumber);
+  (userInput).value = '';
+});
+
+
 guess.addEventListener('click', function() {
   var input = parseInt(userInput.value);
+  document.querySelector('h3').innerText = input;
   if (isNaN(userInput.value)) {
   return alert('Please guess a number');
   }
@@ -24,7 +37,3 @@ guess.addEventListener('click', function() {
 function mysteryNumber() {
   return Math.floor((Math.random() * 100) + 1);
 }
-
-clear.addEventListener('click', function() {
-  userInput.value = '';
-});
