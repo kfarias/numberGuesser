4073264ae88c7c226c4ebbd7237bc78093fe5373
Kenia Farias <11431kenia@gmail.com>
2016-10-11 13:45:54 -0600

    Adds if/else statement and corrects variables

--- scripts.js
+++ scripts.js
@@ -1,12 +1,24 @@
-// var userInput=document.getElementById('#user-input');
-// var guess=document.getElementById('#guess');
-// var clear=document.getElementById('#clear');
-//
-// guess.addEventListener('click', function() {
-//   // alert(userInput.value);
-//   console.log('Hello');
-// });
+
 var mysteryNumber = mysteryNumber();
+var userInput=document.getElementById('user-input');
+var guess=document.getElementById('guess');
+var clear=document.getElementById('clear');
+
+guess.addEventListener('click', function() {
+  var input = parseInt(userInput.value);
+  if (isNaN(userInput.value)) {
+  return alert('Please guess a number')
+  }
+  if (input > mysteryNumber) {
+  return alert('You have guessed too high')
+  }
+  if (input < mysteryNumber) {
+    return alert('You have guessed too low')
+  }
+  if (input === mysteryNumber) {
+  return alert('Correct!!');
+  }
+});
 
 console.log(mysteryNumber);
 
