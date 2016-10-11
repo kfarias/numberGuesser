942cc63f335bad1989edb1247bbe65e50b5436b4
Adam Rice <adamcoltonrice@gmail.com>
2016-10-11 14:55:34 -0600

    clear btn removes input value

--- scripts.js
+++ scripts.js
@@ -1,27 +1,30 @@
-
 var mysteryNumber = mysteryNumber();
-var userInput=document.getElementById('user-input');
-var guess=document.getElementById('guess');
-var clear=document.getElementById('clear');
+var userInput = document.getElementById('user-input');
+var guess = document.getElementById('guess');
+var clear = document.getElementById('clear');
+
+console.log(mysteryNumber);
 
 guess.addEventListener('click', function() {
   var input = parseInt(userInput.value);
   if (isNaN(userInput.value)) {
-  return alert('Please guess a number')
+  return alert('Please guess a number');
   }
   if (input > mysteryNumber) {
-  return alert('You have guessed too high')
+  return alert('You have guessed too high');
   }
   if (input < mysteryNumber) {
-    return alert('You have guessed too low')
+    return alert('You have guessed too low');
   }
   if (input === mysteryNumber) {
   return alert('Correct!!');
   }
 });
 
-console.log(mysteryNumber);
-
 function mysteryNumber() {
   return Math.floor((Math.random() * 100) + 1);
 }
+
+clear.addEventListener('click', function() {
+  userInput.value = '';
+});
